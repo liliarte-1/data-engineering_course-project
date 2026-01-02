a5660b082da93ce4f3fddddfc65a479c47d57dca
lili liarte <alu.156530@usj.es>
2026-01-02 17:41:01 +0100

    cambiox

--- src/ingestion1.py
+++ src/ingestion1.py
@@ -14,8 +14,27 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-urls = ["https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/0201010101.csv", 
-        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/households_dataset.csv"]
+urls = ["https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/economic_sector_province.csv", 
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/death_causes_province.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2008.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2009.csv,"
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2010.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2011.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2012.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2013.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2014.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2015.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2016.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2017.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2018.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2019.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2020.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2021.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2022.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2023.csv",
+        "https://raw.githubusercontent.com/liliarte-1/data-engineering_course-project/refs/heads/main/data_retrieval_simulation/pobmun/data_retrieval_simulation/pobmun/pobmun2024.csv",
+        ]
+        
 
 
 logging.info(f"Started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
